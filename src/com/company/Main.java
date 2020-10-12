34412e11cd7b79ee12e794fe2ea059d15cf62354
Dunay <adream0@gmail.com>
2020-10-12 14:40:54 +0100

    #1 Created JLex class files and java_cup added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,8 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+    //public static void main(String argv[]) throws java.io.IOException {
+     //   Yylex yy = new Yylex(System.in);
+      //  while (yy.yylex() != -1) ;
+    //}
 }
